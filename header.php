6003fddab328e82a92aeda087473ce6298f1ec1e
Billie Matteo <billie_marie@yahoo.com>
2015-10-30 19:06:04 -0700

    Donate button
    Added a snippet if code that will become the donate button. I placed it
    in the hgroup because if we use it other places they will need to be
    styled differently.

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 	<hgroup>
     	<h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
         <h2><?php bloginfo('description'); ?></h2>
+		<div id="donate"> Donate </div> <!-- This will be our donate button crafted by CSS (currently residing in hgroup) -->
     </hgroup>
    
     <div id="mainnav">
