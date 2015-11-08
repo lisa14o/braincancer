5fa5eeb5d9ea7c88bb6115052eecf25d70a313d2
Billie Matteo <billie_marie@yahoo.com>
2015-11-08 09:26:01 -0800

    Changes to footer,header, and styles
    Added social media to header and footer
    Added footer nav manu
    Styled changes in CSS

--- footer.php
+++ footer.php
@@ -1,16 +1,19 @@
 <div id="footer">
 <!--best comment everrrrrrr-->
 
-<p>Footer Area to be defined</p>
-<a href="wp-login.php">Log In</a>
-<?php
-	$footer_nav = array(
-	'theme_location' => 'footer-nav',
-	'container' => 'nav',
-	'container_id' => 'footer-main',
-	'depth' => 1
-	);
-?>
+			<ul class="socialitems">
+            	<li id="twitter"><a href="https://twitter.com/nobraincancer">T</a></li>
+				<li id="facebook"><a href="https://www.facebook.com/braincancer.org">F</a></li>
+				<li id="pintrest"><a href="http://www.pinterest.com/braincancerorg/">P</a></li>
+			</ul>
+            	
+<a href="wp-login.php" id="login" >Log In</a>
+
+<p id="tagline">1 206-000-000 | 1234 5th Ave. Seattle WA 98123 | helpdesk@braincancer.org</p>
+
+<div id="footernav">
+    <?php wp_nav_menu(array("menu" => "Footer")); ?>
+    </div>
 
 </div>
 	
--- header.php
+++ header.php
@@ -14,6 +14,15 @@
 <body>
 
 	<hgroup>
+            
+            <ul class="socialitems">
+            	<li id="twitter"><a href="https://twitter.com/nobraincancer">T</a></li>
+				<li id="facebook"><a href="https://www.facebook.com/braincancer.org">F</a></li>
+				<li id="pintrest"><a href="http://www.pinterest.com/braincancerorg/">P</a></li>
+			</ul>	
+            
+            
+            
             <form role="search" method="get" id="searchform" class="searchform" action="<?php bloginfo('home'); ?>/"><!-- our search button-->
 				<div>
 					<label class="screen-reader-text" for="s"></label>
@@ -22,8 +31,11 @@
 			</form>
 
             <div id="donate"><a href=""><b> Donate </b> </div></a><!-- our donate button-->
+            
+            <div id="menu-social" class="menu-social">
+    
     </hgroup>
    
     <div id="mainnav">
-    <?php wp_nav_menu(array("menu" => "Primary Menu")); ?>
+    <?php wp_nav_menu(array("menu" => "Main")); ?>
     </div>
