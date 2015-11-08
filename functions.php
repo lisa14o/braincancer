2b7ab96d5a2c5f2038c21b029f4f2d568778b6ee
Billie Matteo <billie_marie@yahoo.com>
2015-11-08 15:04:17 -0800

    Removed template printing of the title
    Removed template printing of the title since it is treated differently
    on different pages so now it is manually added in the page content. This
    does not effect navigation or meta data elements.

--- functions.php
+++ functions.php
@@ -6,6 +6,6 @@ add_theme_support('post-thumbnails');
 set_post_thumbnail_size(200,200, true);
 add_post_type_support( 'page', 'excerpt' );
 add_filter( 'wp_title','braincancer_filter_wp_title', 10, 3 );
-
+add_filter( 'wp_title', 'filter_function_name', 10, 2 );
 
 ?>
--- page.php
+++ page.php
@@ -5,7 +5,7 @@
     <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
     
     <article class="halfcol left">
-        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+        
         <?php the_post_thumbnail(); ?>
         
         <p><?php the_content(); ?></p>
