350ccdfee10b5c810154e93cfecc509d6f1d6777
Billie Matteo <billie_marie@yahoo.com>
2015-12-02 08:11:07 -0800

    First Round Mobile Styling
    Added show and hides to donate and search in header.
    Added show and hide to donate and search in mobile nav.
    Added submenu into header.
    Styled first round of mobile nav in CSS

--- header.php
+++ header.php
@@ -4,11 +4,11 @@
     <meta charset="utf-8">
     <title><?php the_title(); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-
-
     <!--[if lt IE 9]>
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
     <![endif]-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+    
 <?php wp_head(); ?>
 </head>
 <body>
@@ -17,7 +17,7 @@
             
                        
             
-            
+            <div class="hidemobile">
             <form role="search" method="get" id="searchform" class="searchform" action="<?php bloginfo('home'); ?>/"><!-- our search button-->
 				<div>
 					<label class="screen-reader-text" for="s"></label>
@@ -26,15 +26,25 @@
 			</form>
 
             <div id="donate"><a href=""><b> Donate </b> </div></a><!-- our donate button-->
-            
-            <div id="menu-social" class="menu-social">
+            </div><!------Close hidemobile------>
+            <div id="menu-social" class="menu-social"></div>
     
     </hgroup>
    
-    <div id="mainnav">
-    <?php wp_nav_menu(array("menu" => "Main")); ?>
+    <div id="mainnav"><?php wp_nav_menu(array("menu" => "Main")); ?></div>
     
-     </div>
+    
+    <div class="showmobile">
+    <div id="donate"><a href=""><b> Donate </b> </a></div><!-- our donate button-->
+            
+    <form role="search" method="get" id="searchform" class="searchform" action="<?php bloginfo('home'); ?>/"><!-- our search button-->
+				<div>
+					<label class="screen-reader-text" for="s"></label>
+					<input type="text" value="" name="s" id="s" placeholder="  Search" />
+				</div>
+			</form>
+     </div><!----close showmbile------>
+     
      <nav class="breadcrumb">
     <?php if( function_exists( 'bcn_display' )){bcn_display();} ?></nav>
     
